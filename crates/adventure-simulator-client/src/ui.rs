4893832bbae00efdff064c97c5d560355c484c5a
barsoosayque <shtoshich@gmail.com>
2026-02-11 16:08:47 +0700

    add client/server status ui

--- crates/adventure-simulator-client/src/ui.rs
+++ crates/adventure-simulator-client/src/ui.rs
@@ -1,4 +1,5 @@
 use adventure_simulator_core::{player::PlayerId, prelude::CharacterController};
+use adventure_simulator_net::lightyear::{connection::client::ClientState, prelude::*};
 use bevy::prelude::*;
 use bevy_flair::prelude::*;
 
@@ -8,12 +9,21 @@ impl Plugin for UiPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(FlairPlugin)
             .add_systems(Startup, setup_ui)
-            .add_systems(Update, update_ui);
+            .add_systems(Update, (update_player_ui, update_connection_ui));
     }
 }
 
 #[derive(Component)]
 struct PositionSpan;
+
+#[derive(Component)]
+struct ServerInfoSpan;
+
+#[derive(Component)]
+struct ClientInfoSpan;
+
+#[derive(Component)]
+struct ClientStatusSpan;
 
 fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
@@ -21,28 +31,77 @@ fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
         NodeStyleSheet::new(asset_server.load("ui.css")),
         children![
             (
-                Name::new("controls-text"),
+                Name::new("controls"),
                 Text::new("WASD to move | Space to jump | Mouse to look around"),
             ),
             (
                 Name::new("position"),
                 Text::new("Position: "),
                 children![(PositionSpan, TextSpan::default())]
+            ),
+            (
+                Name::new("connection"),
+                Node::default(),
+                children![
+                    (
+                        Text::new("Server: "),
+                        children![(ServerInfoSpan, TextSpan::default())]
+                    ),
+                    (
+                        Text::new("Client: "),
+                        children![
+                            (ClientInfoSpan, TextSpan::default()),
+                            (ClientStatusSpan, ClassList::default(), TextSpan::default())
+                        ]
+                    ),
+                ]
             )
         ],
     ));
 }
 
-fn update_ui(
-    player: Single<(&Transform, &PlayerId), With<CharacterController>>,
-    mut text_query: Query<&mut TextSpan, With<PositionSpan>>,
+fn update_player_ui(
+    player: Single<(Ref<Transform>, &PlayerId), With<CharacterController>>,
+    mut q_position_span: Query<&mut TextSpan, With<PositionSpan>>,
 ) {
-    let (Transform { translation, .. }, &PlayerId(player_id)) = player.into_inner();
+    let (transform, &PlayerId(player_id)) = player.into_inner();
 
-    for mut text in &mut text_query {
-        text.0 = format!(
-            "{:.1} {:.1} {:.1}",
-            translation.x, translation.y, translation.z
-        );
+    if transform.is_changed() {
+        let translation = transform.translation;
+        for mut text in &mut q_position_span {
+            text.0 = format!(
+                "{:.1} {:.1} {:.1}",
+                translation.x, translation.y, translation.z
+            );
+        }
     }
+}
+
+fn update_connection_ui(
+    player: Single<(&PeerAddr, &LocalAddr, &Client, Option<&Disconnected>), Changed<Client>>,
+    mut spans: ParamSet<(
+        Single<&mut TextSpan, With<ServerInfoSpan>>,
+        Single<&mut TextSpan, With<ClientInfoSpan>>,
+        Single<(&mut TextSpan, &mut ClassList), With<ClientStatusSpan>>,
+    )>,
+) {
+    let (peer, local, client, disconnected) = player.into_inner();
+
+    spans.p0().0 = peer.0.to_string();
+    spans.p1().0 = local.0.to_string();
+    let (status_text, status_class) = match (client.state, disconnected) {
+        (ClientState::Connected, _) => ("\nConnected".to_string(), "success"),
+        (ClientState::Connecting, _) => ("\nConnecting...".to_string(), "primary"),
+        (ClientState::Disconnecting, _) => ("\nDisconnecting..".to_string(), "primary"),
+        (
+            ClientState::Disconnected,
+            Some(Disconnected {
+                reason: Some(reason),
+            }),
+        ) => (format!("\nDisconnected:\n{reason}"), "error"),
+        (ClientState::Disconnected, _) => ("\nDisconnected (no reason)".to_string(), "error"),
+    };
+
+    spans.p2().0 .0 = status_text;
+    *spans.p2().1 = ClassList::new(status_class);
 }
