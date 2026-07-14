6211954eebaa4c06fe5492b1ab66eff373dc040c
Adler Halbe <halbe@adventuresim.org>
2026-07-14 12:12:37 -0700

    Align inventory arrows along panel edges

--- crates/strategic-web/src/templates/layout.rs
+++ crates/strategic-web/src/templates/layout.rs
@@ -103,7 +103,7 @@ fn page_shell(title: &str, header: Markup, content: Markup, theme: &str) -> Mark
                 // Shared CSS
                 link rel="stylesheet" href="/static/css/reset.css";
                 link rel="stylesheet" href="/static/css/layout.css?v=location-tabs-1";
-                link rel="stylesheet" href="/static/css/components.css?v=inventory-targets-1";
+                link rel="stylesheet" href="/static/css/components.css?v=inventory-arrow-rails-2";
 
                 // Datastar
                 script type="module" src="https://cdn.jsdelivr.net/gh/starfederation/datastar/bundles/datastar.js" {}
