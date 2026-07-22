f3fad5a1ec130ed4d5c819ffbfe570e103eb83dd
Adler Halbe <halbe@adventuresim.org>
2026-07-21 19:39:58 -0700

    Fix grouped food disclosure behavior

--- crates/strategic-web/src/templates/layout.rs
+++ crates/strategic-web/src/templates/layout.rs
@@ -158,7 +158,7 @@ fn page_shell(title: &str, header: Markup, content: Markup, scripts: ScriptProfi
                 link rel="stylesheet" href="/static/css/reset.css";
                 link rel="stylesheet" href="/static/css/layout.css?v=subtle-material-lines-1-dialogue-source-icons-2";
                 link rel="stylesheet" href="/static/css/components.css?v=lowercase-display-type-1";
-                link rel="stylesheet" href="/static/css/strategic.css?v=strategic-ui-overhaul-4-paper-map-2-data-license-1-grouped-alcohol-2-rest-supplies-1-dialogue-pane-1";
+                link rel="stylesheet" href="/static/css/strategic.css?v=strategic-ui-overhaul-4-paper-map-2-data-license-1-grouped-alcohol-2-grouped-food-1-rest-supplies-1-dialogue-pane-1";
                 link rel="stylesheet" href="/static/css/utilities.css?v=strategic-ui-overhaul-1";
 
                 // Datastar
