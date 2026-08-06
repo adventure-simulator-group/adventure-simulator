f1c73ce483fcdbeeda8c8876238a39da3f8266c1
Adler Halbe <halbe@adventuresim.org>
2026-08-05 21:47:04 -0700

    Regenerate hostile surrender bindings

--- crates/adventuresim-stdb-client/src/hostile_surrender_outcome_type.rs
+++ crates/adventuresim-stdb-client/src/hostile_surrender_outcome_type.rs
@@ -7,18 +7,12 @@ use spacetimedb_sdk::__codegen::{self as __sdk, __lib, __sats, __ws};
 #[derive(__lib::ser::Serialize, __lib::de::Deserialize, Clone, PartialEq, Debug)]
 #[sats(crate = __lib)]
 #[derive(Copy, Eq, Hash)]
-pub enum HostileGroupDisposition {
-    Active,
+pub enum HostileSurrenderOutcome {
+    Refused,
 
-    Defeated,
-
-    DrivenOff,
-
-    Surrendered,
-
-    Captured,
+    Accepted,
 }
 
-impl __sdk::InModule for HostileGroupDisposition {
+impl __sdk::InModule for HostileSurrenderOutcome {
     type Module = super::RemoteModule;
 }
